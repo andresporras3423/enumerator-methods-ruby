56993dfc6c13485d4db5f16a127088e5f47bae78
andresporras3423 <andresporras3423@gmail.com>
2020-02-18 15:33:22 -0500

    added my__each_with_index

--- enumerator-methods.rb
+++ enumerator-methods.rb
@@ -14,4 +14,19 @@ module Enumerable
       x+=1
     end
   end
+  def my_each_with_index
+    return to_enum unless block_given?
+    x=0
+    array = nil
+    self_class = self.class
+    if self_class == Array
+      array = self
+    else
+      array = to_a
+    end
+    while x < array.length
+      yield array[x], x
+      x+=1
+    end
+  end
 end
