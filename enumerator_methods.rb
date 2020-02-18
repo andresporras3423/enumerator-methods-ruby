7411b5fd91292c3925f60e3156967eca5df58ba2
andresporras3423 <andresporras3423@gmail.com>
2020-02-18 16:23:00 -0500

    solving stickler errors

--- enumerator_methods.rb
+++ enumerator_methods.rb
@@ -1,7 +1,6 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-
     x = 0
     array = to_a
     while x < array.length
@@ -10,9 +9,7 @@ module Enumerable
     end
   end
   def my_each_with_index
-    
     return to_enum unless block_given?
-
     x = 0
     array = to_a
     while x < array.length
