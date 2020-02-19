da28b2912bc345a3cd6beb505e43390eb23ccfed
andresporras3423 <andresporras3423@gmail.com>
2020-02-19 16:06:19 -0500

    solving stickler errors

--- main.rb
+++ main.rb
@@ -3,11 +3,11 @@ require './enumerator_methods'
 def multiply_els(arr)
   rand_method = rand(1..3)
   case rand_method
-  when 1 #by block
+  when 1 # by block
     puts arr.my_inject {|total, a| total * a}
-  when 2 #by symbol
+  when 2 # by symbol
     puts arr.my_inject(:*)
-  when 3 #by proc
+  when 3 # by proc
     by_proc = proc {|total, a| total * a}
     puts arr.my_inject (by_proc)
   end
@@ -43,44 +43,3 @@ puts (6..10).my_inject(5){ |sum, n| sum - n }
 puts (6..10).my_inject(5,:-)
 puts 'EXAMPLE MULTIPLY_ELS' 
 multiply_els([2,4,5])
-# string1='EXAMPLE MY_EACH'
-# string1+="\nI want a hamburguer"
-# string1+="\nI want a pizza"
-# string1+="\nI want a hot dog"
-# string1+="\nI want a hamburguer"
-# string1+="\nI want a sushi"
-# string1+="\nEXAMPLE MY_EACH_WITH_INDEX"
-# string1+="\nproduct number 1 is: hamburguer"
-# string1+="\nstring1+=product number 2 is: pizza"
-# string1+="\nproduct number 3 is: hot dog"
-# string1+="\nproduct number 4 is: hamburguer"
-# string1+="\nproduct number 5 is: sushi"
-# string1+="\nEXAMPLE MY_SELECT"
-# string1+="\n2"
-# string1+="\n4"
-# string1+="\n6"
-# string1+="\nEXAMPLE MY_ALL?"
-# string1+="\ntrue"
-# string1+="\nEXAMPLE ANY?"
-# string1+="\nfalse"
-# string1+="\nEXAMPLE NONE?"
-# string1+="\ntrue"
-# string1+="\nEXAMPLE MY_COUNT"
-# string1+="\n2"
-# string1+="\nEXAMPLE MY_MAP"
-# string1+="\nhamburguer hamburguer"
-# string1+="\npizza pizza"
-# string1+="\nhot dog hot dog"
-# string1+="\nhamburguer hamburguer"
-# string1+="\nsushi sushi"
-# string1+="\nEXAMPLE MY_INJECT"
-# string1+="\n-35"
-# string1+="\n-28"
-# string1+="\n-28"
-# string1+="\n-28"
-# string1+="\n-35"
-# string1+="\n-35"
-# string1+="\n-35"
-# string1+="\nEXAMPLE MULTIPLY_ELS"
-# string1+="\n40"
-# puts string1
