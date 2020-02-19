63e8ea8338709d431d4ae944951a15f9b0577759
andresporras3423 <andresporras3423@gmail.com>
2020-02-19 16:30:55 -0500

    solving stickler errors

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ def multiply_els(arr)
   rand_method = rand(1..3)
   case rand_method
   when 1 # by block
-    puts arr.my_inject{ |total, a| total * a }
+    return arr.my_inject { |total, a| total * a }
   when 2 # by symbol
     puts arr.my_inject(:*)
   when 3 # by proc
