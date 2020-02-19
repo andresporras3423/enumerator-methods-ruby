4519ae79a98ee43b8c832efc0dabe6b7df88eac3
andresporras3423 <andresporras3423@gmail.com>
2020-02-19 16:44:34 -0500

    solving stickler errors

--- main.rb
+++ main.rb
@@ -13,33 +13,33 @@ def multiply_els(arr)
   end
 end
 
-lista = ['hamburguer', 'pizza', 'hot dog', 'hamburguer', 'sushi']
-puts 'EXAMPLE MY_EACH'
-lista.my_each do |lis|
-  puts "I want a #{lis}"
-end
-puts 'EXAMPLE MY_EACH_WITH_INDEX'
-lista.my_each_with_index { |n, i| puts "product number #{i + 1} is: #{n}" }
-puts 'EXAMPLE MY_SELECT'
-puts ([1,2,3,4,5,6].my_select) { |n| n % 2 == 0 }
-puts 'EXAMPLE MY_ALL?' 
-puts lista.my_all?{|x| x.length > 3}
-puts 'EXAMPLE ANY?' 
-puts [nil, nil].my_any?
-puts 'EXAMPLE NONE?' 
-puts [lista].none?{|x| x == "pizzas"}
-puts 'EXAMPLE MY_COUNT' 
-puts lista.my_count("hamburguer")
-puts 'EXAMPLE MY_MAP' 
-puts lista.my_map{|x| x+" "+x}
-puts 'EXAMPLE MY_INJECT' 
-puts (5..10).my_inject { |sum, n| sum - n } 
-c =  proc { |sum, n| sum - n } 
-puts (6..10).my_inject(c)
-puts (6..10).my_inject{ |sum, n| sum - n } 
-puts (6..10).my_inject(:-)
-puts (6..10).my_inject(5,c)
-puts (6..10).my_inject(5){ |sum, n| sum - n } 
-puts (6..10).my_inject(5,:-)
-puts 'EXAMPLE MULTIPLY_ELS' 
+# lista = ['hamburguer', 'pizza', 'hot dog', 'hamburguer', 'sushi']
+# puts 'EXAMPLE MY_EACH'
+# lista.my_each do |lis|
+#   puts "I want a #{lis}"
+# end
+# puts 'EXAMPLE MY_EACH_WITH_INDEX'
+# lista.my_each_with_index { |n, i| puts "product number #{i + 1} is: #{n}" }
+# puts 'EXAMPLE MY_SELECT'
+# puts ([1,2,3,4,5,6].my_select) { |n| n % 2 == 0 }
+# puts 'EXAMPLE MY_ALL?' 
+# puts lista.my_all?{|x| x.length > 3}
+# puts 'EXAMPLE ANY?' 
+# puts [nil, nil].my_any?
+# puts 'EXAMPLE NONE?' 
+# puts [lista].none?{|x| x == "pizzas"}
+# puts 'EXAMPLE MY_COUNT' 
+# puts lista.my_count("hamburguer")
+# puts 'EXAMPLE MY_MAP' 
+# puts lista.my_map{|x| x+" "+x}
+# puts 'EXAMPLE MY_INJECT' 
+# puts (5..10).my_inject { |sum, n| sum - n } 
+# c =  proc { |sum, n| sum - n } 
+# puts (6..10).my_inject(c)
+# puts (6..10).my_inject{ |sum, n| sum - n } 
+# puts (6..10).my_inject(:-)
+# puts (6..10).my_inject(5,c)
+# puts (6..10).my_inject(5){ |sum, n| sum - n } 
+# puts (6..10).my_inject(5,:-)
+# puts 'EXAMPLE MULTIPLY_ELS' 
 puts multiply_els([2,4,5])
